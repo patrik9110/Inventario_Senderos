f8e27c81c59cc29007c76934aa035ba847e0e055
patrik9110 <30991803+patrik9110@users.noreply.github.com>
2018-12-23 01:48:50 -0300

    I'm awesome

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ wb1 = load_workbook("INVENTARIO.xlsx")
 wb2 = load_workbook("VENTAS.xlsx")
 inventario = wb1["Hoja 1"]
 ventas = wb2["Hoja 1"]
-print(ventas["A2"].value)
+'''row = ""
+c = 0
 
-''''def main():
+for row in ventas["A"]:
+    c += 1
+
+print(row.value)
+print(ventas["B"+str(c)].value)'''
+
+
+
+def main():
 
     print("Bienvenido/a")
 
@@ -16,28 +25,36 @@ print(ventas["A2"].value)
 
     def agregarItem():
         itemNuevo = input("Item Nuevo: ")
-        for itemChecker in iItem:
+        for itemChecker in inventario["A"]:
+            rowCounter = 0
             if itemChecker == itemNuevo:
                 print(itemChecker + " ya esta en el inventario, que desea hacer: ")
-                accion2 = input("[R]enombrar \n[A]gregar mas unidades de " + itemChecker + "\n[Ca]ncelar \n.. ")
+                accion2 = input("[R]eintentar \n[A]gregar mas unidades de " + itemChecker + "\n[Ca]ncelar \n.. ")
                 if accion2.upper() == "R":
                     agregarItem()
                 elif accion2.upper() == "A":
                     masUnidades = input("Cuantas nuevas unidades hay de " + itemChecker + "? ")
-                    int(iCantidad[itemChecker.index()]) + int(masUnidades)
+                    inventario["B"+str(rowCounter)] = str(int(masUnidades) + int(inventario["B"+str(rowCounter)]))
+                    print("Se actualizo la cantidad de " + itemChecker + " a " + inventario["B"+str(rowCounter)])
                 elif accion2.upper() == "CA":
-                    inicio()
+                    main()
             if itemChecker != itemNuevo:
-                iItem.append(itemNuevo)
+                colLen = 1
+                for i in inventario["A"]:
+                    colLen += 1
                 cantidadNueva = input("Cuantas unidades? ")
-                int(iCantidad[itemNuevo.index()]) + int(cantidadNueva)
-                nuevoItemVentaSS = input("A cuanto se vende? ")
-                iPrecioVenta[itemNuevo.index()] = nuevoItemVentaSS
-
+                inventario["A"+str(colLen)] = itemNuevo
+                inventario["B"+str(colLen)] = cantidadNueva
+                precioNuevo = input("A cuanto se va a vender? ")
+                inventario["C"+str(colLen)] = precioNuevo
+                print(itemNuevo + " agregado al inventario!")
+            rowCounter += 1
 
 
     if accion.upper() == "C":
         agregarItem()
 
 main()
-'''
+
+wb1.save("INVENTARIO.xlsx")
+wb2.save("VENTAS.xlsx")
