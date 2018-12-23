1a4ab4044cad9d577c77e78dc6a82a996ec13139
patrik9110 <30991803+patrik9110@users.noreply.github.com>
2018-12-23 18:07:43 -0300

    hehe checking this out

--- app.py
+++ app.py
@@ -5,54 +5,69 @@ wb1 = load_workbook("INVENTARIO.xlsx")
 wb2 = load_workbook("VENTAS.xlsx")
 inventario = wb1["Hoja 1"]
 ventas = wb2["Hoja 1"]
-'''row = ""
-c = 0
-
-for row in ventas["A"]:
-    c += 1
-
-print(row.value)
-print(ventas["B"+str(c)].value)'''
-
 
 
 def main():
 
     print("Bienvenido/a")
 
-    accion = input("Que queres hacer? \n[C]ompra \n[V]enta \n[E]ditar \n[M]irar \n.. ")
+    accion = input("Que queres hacer? \n[C]ompra \n[V]enta \n[E]ditar \n[M]irar \n[S]alir \n.. ")
 
 
     def agregarItem():
+
+        def usado():
+            print(itemChecker.value + " ya esta en el inventario, que desea hacer: ")
+            accion2 = input("[R]eintentar \n[A]gregar mas unidades de " + itemChecker.value + "\n[I]nicio \n[S]alir \n.. ")
+            if accion2.upper() == "R":
+                agregarItem()
+            elif accion2.upper() == "A":
+                masUnidades = input("Cuantas nuevas unidades nuevas hay de " + itemChecker.value + "? ")
+                precioProducto = inventario["B"+str(rowCounter)]
+                inventario["B"+str(rowCounter)] = int(masUnidades) + int(precioProducto.value)
+                print("Se actualizo la cantidad de " + itemChecker.value + " a " + str(inventario["B"+str(rowCounter)].value))
+                main()
+            elif accion2.upper() == "I":
+                main()
+            elif accion2.upper() == "S":
+                quit()
+        def nuevo():
+            colLen = 1
+            for i in inventario["A"]:
+                colLen += 1
+            cantidadNueva = input("Cuantas unidades? ")
+            inventario["A"+str(colLen)] = itemNuevo
+            inventario["B"+str(colLen)] = cantidadNueva
+            precioNuevo = input("A cuanto se va a vender? ")
+            inventario["C"+str(colLen)] = precioNuevo
+            print(itemNuevo + " agregado al inventario!")
+            sigue2 = input("Algo mas para agregar al inventario? \n[Si] \n[I]nicio \n[S]alir")
+            if sigue2.upper() == "SI":
+                wb1.save("INVENTARIO.xlsx")
+                agregarItem()
+            elif sigue2.upper() == "I":
+                wb1.save("INVENTARIO.xlsx")
+                main()
+            elif sigue2.upper() == "S":
+                wb1.save("INVENTARIO.xlsx")
+                quit()
+
         itemNuevo = input("Item Nuevo: ")
+        itemChecker = " "
         for itemChecker in inventario["A"]:
-            rowCounter = 0
-            if itemChecker == itemNuevo:
-                print(itemChecker + " ya esta en el inventario, que desea hacer: ")
-                accion2 = input("[R]eintentar \n[A]gregar mas unidades de " + itemChecker + "\n[Ca]ncelar \n.. ")
-                if accion2.upper() == "R":
-                    agregarItem()
-                elif accion2.upper() == "A":
-                    masUnidades = input("Cuantas nuevas unidades hay de " + itemChecker + "? ")
-                    inventario["B"+str(rowCounter)] = str(int(masUnidades) + int(inventario["B"+str(rowCounter)]))
-                    print("Se actualizo la cantidad de " + itemChecker + " a " + inventario["B"+str(rowCounter)])
-                elif accion2.upper() == "CA":
-                    main()
+            rowCounter = 1
+            if itemChecker.value == itemNuevo:
+                usado()
             if itemChecker != itemNuevo:
-                colLen = 1
-                for i in inventario["A"]:
-                    colLen += 1
-                cantidadNueva = input("Cuantas unidades? ")
-                inventario["A"+str(colLen)] = itemNuevo
-                inventario["B"+str(colLen)] = cantidadNueva
-                precioNuevo = input("A cuanto se va a vender? ")
-                inventario["C"+str(colLen)] = precioNuevo
-                print(itemNuevo + " agregado al inventario!")
+                nuevo()
             rowCounter += 1
 
 
     if accion.upper() == "C":
         agregarItem()
+
+    if accion.upper() == "S":
+        quit()
 
 main()
 
